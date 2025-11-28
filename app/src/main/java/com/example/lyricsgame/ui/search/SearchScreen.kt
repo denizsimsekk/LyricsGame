d0cf7827f333b7951842887cb1b03c15edcfaad6
denizsimsek <ddenizsimsekk@gmail.com>
2025-11-28 19:02:20 +0300

    search screen

--- app/src/main/java/com/example/lyricsgame/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/lyricsgame/ui/search/SearchScreen.kt
@@ -2,8 +2,16 @@ package com.example.lyricsgame.ui.search
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.example.lyricsgame.ui.base.BaseScreen
 
 @Composable
 fun SearchScreen(navController: NavController) {
+    BaseScreen(isTopBarShown = true, topBarTitle = "Search Your Favourite Artist", navController = navController) {
+
+    }
+}
+
+@Composable
+private fun MainContent() {
 
 }
